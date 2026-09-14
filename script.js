f97dd0a7d9b120b192090dab7fe10a9308afb90b
gabrielabragaoliveira-ship-it <gabriela.braga.oliveira@escola.pr.gov.br>
2026-09-14 11:23:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+const botao = document.querySelectorAll("button");  
+      botoes.forEach(function(botao) {
+         let curtiu = false;
+      botao.addEventListner("click", botaoClicado);
+      function botaoClicao() {
+         console.log("fui clicado");
+         let texto = botao.querySelctor("span");
+        if(curtiu === false) {
+         texto.textContent++;
+        curtiu = true;
+        } else{
+          texto.textContent--;
+          curtiu = false;
+        }
+      }
+      })
